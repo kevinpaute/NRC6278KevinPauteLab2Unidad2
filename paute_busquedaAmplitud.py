a77db57f0bb90b1d3094624742cc1c1218b61c78
Kevin Paute <knpaute@espe.edu.ec>
2022-05-29 17:18:05 -0500

    Corrección de comentarios

--- paute_busquedaAmplitud.py
+++ paute_busquedaAmplitud.py
@@ -1,14 +1,31 @@
+#Se importa la clase queue de python
 from queue import Queue 
 
 #Se define la clase Grafo 
 class Grafo:
+    '''
+    Clase que permite representar un grafo con listas de adyacencia
+
+        Atributos:
+
+            m_numero_de_nodos (int): Número de nodos del grafo
+            m_nodos (list): Lista de nodos del grafo
+            m_dirigido (boolean): Si el grafo es dirigido o no
+            m_lista_adyacencia (dict): Diccionario que representa la lista de adyacencia
+
+        Metodos:
+            agregar_arista(self,nodo1,nodo2,peso=1): Agrega una arista al grafo
+            imprmir_lista_adyacencia(self): Imprime la lista de adyacencia en forma de matriz
+            recorrido_bfs(self,nodo_inicial): Recorre el grafo en amplitud 
+
+    '''      
     #Constructor
     def __init__(self,numero_nodos,dirigido=True):
         '''
         Constructor de la clase Grafo
             Parámetros:
-            numero_nodos (int): Número de nodos del grafo
-            dirigido (boolean): Si el grafo es dirigido o no
+                numero_nodos (int): Número de nodos del grafo
+                dirigido (boolean): Si el grafo es dirigido o no
         '''
         #Numero de nodos
         self.m_numero_nodos = numero_nodos
@@ -29,9 +46,9 @@ class Grafo:
         '''
         Función que permite agregar una arista al grafo.
             Parámetros:
-            nodo1 (int): Nodo de inicio
-            nodo2 (int): Nodo de fin
-            peso (int): Peso de la arista
+                nodo1 (int): Nodo de inicio
+                nodo2 (int): Nodo de fin
+                peso (int): Peso de la arista
         ''' 
         #Agrega la arista al nodo 1 del grafo
         self.m_lista_adyacencia[nodo1].add((nodo2,peso)) 
@@ -42,9 +59,7 @@ class Grafo:
             self.m_lista_adyacencia[nodo2].add((nodo1,peso))  
 
     def imprmir_lista_adyacencia(self): 
-        '''
-        Funcion que imprime la lista de adyacencia en forma de matriz de adyacencia
-        '''
+        '''Funcion que imprime la lista de adyacencia en forma de matriz de adyacencia'''
         #Recorre la lista de adyacencia
         for clave in self.m_lista_adyacencia.keys(): 
             #Imprime el nodo y la lista de adyacencia de cada nodo en forma de matriz 
@@ -54,7 +69,7 @@ class Grafo:
         '''
         Función que permite recorrer el grafo en amplitud
             Parámetros:
-            nodo_inicial (int): Nodo inicial del recorrido
+                nodo_inicial (int): Nodo inicial del recorrido
         '''
         #Inicializar una lista para los nodos visitados
         nodos_visitados = set()
@@ -82,6 +97,7 @@ class Grafo:
                     nodos_visitados.add(nodo_siguiente)
                     
 if __name__ == '__main__':
+    '''Función principal'''
     
     #Se crea o instancia el grafo con 5 nodos y no dirigido
     print("***** Grafo no dirigido *****")
@@ -105,5 +121,5 @@ if __name__ == '__main__':
     grafo.imprmir_lista_adyacencia()
     #Imprime el recorrido en amplitud
     print("\nRecorrido primero en anchura o amplitud")
-    #Recorrido a partir del vertice 2 
-    grafo.recorrido_bfs(2)
+    #Recorrido a partir del vertice 0
+    grafo.recorrido_bfs(0)
